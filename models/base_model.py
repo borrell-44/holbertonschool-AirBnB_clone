e57eee46aa59f0d6253691b7af5d17077503fa0e
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-28 18:00:10 +0000

    Committing task 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,23 +3,37 @@
 """ file Comments """
 
 import uuid
-from datetime import date
+from datetime import datetime
 
 
 class BaseModel(object):
 
     """ class Comments """
 
-    def __init__(self):
+    def __init__(self, my_number=0, name="", updated_at=None,
+                 id="", created_at=None):
         """ init Comments """
         self.id = str(uuid.uuid4())
+        date_now = datetime.now()
+        self.created_at = date_now
+        self.updated_at = date_now
 
     def __str__(self):
         """ str Comments """
-        name = "[" + str(self.__name__) + "]"
-        nid = "(" + self.id + ")"
-        dic = self.__dict__
-        return " ".join(name, nid, dic)
+        name = "[" + str(type(self).__name__) + "]"
+        nid = "(" + str(self.id) + ")"
+        dic = str(self.__dict__)
+        return name + " " + nid + " " + dic
 
-    def created_at(self):
-        """ created_at Comments """
+    def to_dict(self):
+        """ to_dict Comments """
+        dic = self.__dict__.copy()
+        dic["__class__"] = str(type(self).__name__)
+        dic["updated_at"] = self.updated_at.isoformat()
+        dic["created_at"] = self.created_at.isoformat()
+        return dic
+
+    def save(self):
+        """ save Comments """
+        date_now = datetime.now()
+        self.updated_at = date_now
