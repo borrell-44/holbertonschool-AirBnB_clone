bc97c6f37cd7a3eb284a7142c179be08762aa882
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-04 08:59:23 +0000

    Committing test file engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" File Storage Tests for the City class  """
+
+from models.engine.file_storage import FileStorage
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """ File Storage tests """
+
+    def test_all_func(self):
+        """ test if the all func returns the objects private var """
+        obj = FileStorage()
+        self.assertEqual(obj.all(), {})
+
+    if __name__ == "__main__":
+        unittest.main()
