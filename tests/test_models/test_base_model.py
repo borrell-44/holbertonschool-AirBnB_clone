2abcde323822a6e52abfc21967ecec6d89f3b992
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-04 06:50:02 +0000

    Adding test for save function

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Unit test for Amenity class"""
+""" Tests for the BaseModel class  """
 
 from models.base_model import BaseModel
+from os.path import exists
 import unittest
 
 
 class TestBaseModel(unittest.TestCase):
-    """Base_model test"""
+    """ Base_model tests """
+
+    def test_base_save(self):
+        """ testing the save function """
+        obj = BaseModel()
+        obj.save()
+        self.assertEqual(exists("file.json"), True)
 
     if __name__ == "__main__":
         unittest.main()
