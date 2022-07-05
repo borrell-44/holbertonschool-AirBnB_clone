19cff22b3560f80c41a544991b6c6e7de72f2f75
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-05 13:28:39 +0000

    Adding multiple tests for task 5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,11 +15,33 @@ class TestFileStorage(unittest.TestCase):
         obj = FileStorage()
         self.assertEqual((obj.__class__).__name__, "FileStorage")
 
-    def test_something(self):
-        """ test somehting """
+    def test_new(self):
+        """ test the instance new """
         file = FileStorage()
         base = BaseModel()
         self.assertEqual(file.new(base), None)
+
+    def test_save(self):
+        """ test the instance save """
+        obj = FileStorage()
+        self.assertEqual(obj.save(), None)
+
+    def test_reload(self):
+        """ test the instance reload """
+        obj = FileStorage()
+        self.assertEqual(obj.reload(), None)
+
+    def test_save_base(self):
+        """ test the instance save of the BaseModel class """
+        obj = BaseModel()
+        self.assertEqual(obj.save(), None)
+
+    def test_init_base(self):
+        """ test init of BaseModel """
+        obj = BaseModel()
+        other = BaseModel()
+        obj.name = "Juannito Perez"
+        self.assertEqual(other.__init__(obj), None)
 
     if __name__ == "__main__":
         unittest.main()
